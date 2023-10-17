aa7f83af7e2a331aba4d80e9dd81eb7bdfba8565
safwan3848 <shaikhsafwan0404@gmail.com>
2023-10-17 11:04:13 +0530

    counter react app

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  let [counter, setCounter] = useState(0);
+
+  const addValue = () => {
+    if (counter < 20) {
+      setCounter(counter + 1);
+    }
+  };
+
+  const removeValue = () => {
+    if (counter > 0) {
+      setCounter(counter - 1);
+    }
+  };
+
+  return (
+    <>
+      <h1>safwan shaikh</h1>
+      <h2>counter value : {counter}</h2>
+
+      <button onClick={addValue}>Add Value</button>
+      <br />
+      <button onClick={removeValue}>Remove Value</button>
+    </>
+  );
+}
+
+export default App;
